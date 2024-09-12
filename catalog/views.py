991e8e70dbc6e1dee7f5f7ad32af79568b2be1f7
Maria Ivanova <moi066@mail.ru>
2024-09-12 13:41:29 +0300

    реализован функционал

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        # Вывод в консоль для отладки
+        print(f"POST запрос получен: Имя={name}, Телефон={phone}, Сообщение={message}")
+
+        # Возвращение ответа
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+    return render(request, 'catalog/contacts.html')
