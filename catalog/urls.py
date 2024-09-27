0a84be4ff5eee0f84dc4696c9d57b60489100ec6
Maria Ivanova <moi066@mail.ru>
2024-09-27 10:43:50 +0300

    добавлены страницы с главным меню и карточкой товара. Выделен базовый шаблон

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, products_list, products_detail
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("home/", home, name="home"),
     path("contacts/", contacts, name="contacts"),
+    path("base/", products_list, name='products_list'),
+    path("catalog/<int:pk>/", products_detail, name='products_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from catalog.models import Product
 
 
 def home(request):
@@ -23,3 +25,19 @@ def contact(request):
         # Возвращение ответа
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, "catalog/contacts.html")
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'products_list.html', context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'products_detail.html', context)
